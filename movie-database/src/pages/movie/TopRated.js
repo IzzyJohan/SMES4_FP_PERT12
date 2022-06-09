77d5ba99ee51375429a806d3f240c15bd6aad3af
Izzy Rizky <izzyjohan88@gmail.com>
2022-06-09 21:31:47 +0700

    Tugas Frontend Pertemuan 12

--- movie-database/src/pages/movie/TopRated.js
+++ movie-database/src/pages/movie/TopRated.js
@@ -2,12 +2,9 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Movies from "../../components/Movies/Movies.module";
 import Hero from "../../components/Hero/Hero.module"; 
+import ENDPOINTS from "../../components/utils/constants/endpoints";
  
 function TopRated() {
-  // Simpan API_KEY dan URL ke variabel
-  const API_KEY = process.env.REACT_APP_API_KEY;
-  const URL = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`; 
-
   // Membuat state movies
   const [movies, setMovies] = useState([]);  
 
@@ -19,7 +16,7 @@ function TopRated() {
 
   async function getTopRated() {
         // Fetch data dari Axios
-        const response = await axios(URL);
+        const response = await axios(ENDPOINTS.TOPRATED);
 
         // Simpan data ke state movies
         setMovies(response.data.results);
